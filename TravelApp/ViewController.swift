ba92073e48387a43c7c0496ad328a1bad48e0d48
AltynbekDinmukhamed <a.dimash.a_03@mail.ru>
2023-12-13 15:00:52 +0600

    Initial Commit

---
+++ TravelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelApp
+//
+//  Created by Димаш Алтынбек on 13.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
